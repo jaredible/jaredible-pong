ff668ea64bad3fa341d93311b2d40ef2670a908b
Jared Diehl <jareddiehl96@gmail.com>
2020-01-23 09:36:30 -0600

    Updated

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 5000
+'use strict'
 
-app.use(express.static(__dirname, {
-    dotfiles: 'allow'
-}))
+const express = require('express')
+
+const PORT = process.env.PORT || 5000
+const HOST = '0.0.0.0'
+
+const app = express()
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 app.listen(PORT, () => {
-    console.log(`Pong app listening on port ${PORT}!`)
+    console.log(`Running at http://${HOST}:${PORT}`)
 })
