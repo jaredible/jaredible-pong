6d4592a77ce7134e38b849eb277c54ea3944809a
Jared Diehl <jareddiehl96@gmail.com>
2020-01-23 03:22:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.use(express.static(__dirname, {
+    dotfiles: 'allow'
+}))
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
